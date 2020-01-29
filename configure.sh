afc4e87f7a7b863566e4326a19e50f8f331169ed
Asher <ashermancinelli@gmail.com>
2020-01-29 23:54:58 +0000

    auto installs vimplug if not found

--- configure.sh
+++ configure.sh
@@ -4,6 +4,9 @@ cp vimrc_base ~/.vimrc
 
 [ -d ~/.vim ] || mkdir ~/.vim
 
+[ -d ~/.vim/autoload ] || curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 [ -d ~/.vim/templates ] || mkdir ~/.vim/templates
 
 for i in $(ls templates)
