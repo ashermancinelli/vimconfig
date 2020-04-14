8e75183369189ae257d2a0cdff36ad28f38fa9d8
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2020-04-14 07:50:11 -0700

    adding tags creation

--- configure.sh
+++ configure.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-cp vimrc_base ~/.vimrc
-
 [ -d ~/.vim ] || mkdir ~/.vim
 
 if [ ! -d ~/.vim/autoload ]
@@ -22,23 +20,6 @@ for i in $(ls *.vim)
 do
     cp $i ~/.vim/$i
 done
-
-type go
-if [ $? -gt 0 ]; then
-    echo 'Go not found... Not installing prologic/ed'
-else
-    echo
-    echo Installing prologic/ed...
-    echo
-
-    test -d external || mkdir external
-    pushd external
-    git clone https://github.com/prologic/ed
-    cd ed
-    go build
-    cp ed ~/.local/bin
-    popd
-fi
 
 type tmux
 if [ $? -ne 0 ]; then
@@ -84,22 +65,36 @@ if [ $? -ne 0 ]; then
     popd; popd
 fi
 
-echo
-echo Setting up shell rc
-echo
-case "$SHELL" in
-    *zsh )
-        echo
-        echo Found Zshell as default!
-        echo
-        ;;
-    * )
-        echo
-        echo Zsh not found... Creating RC file to start Zsh on login.
-        echo Not loading defaults.
-        echo
-        cat redirectrc > "$HOME/.$(basename $SHELL)rc"
-        ;;
-esac
+rm ~/.vimrc
 
-cat baserc zshrc > $HOME/.zshrc
+which ctags
+if [ $? -ne 0 ]; then
+    echo
+    echo Ctags not found...
+    echo
+else
+    echo
+    echo Generating Ctags...
+    echo
+    [ -f ./tags ] && rm ./tags
+    touch ./tags
+    while read pth
+    do
+        find $pth -name '*.h' | \
+             ctags -f ./tags \
+             --verbose \
+             --append \
+             --c++-kinds=+p \
+             --fields=+iaS \
+             --extra=+q \
+             --language-force=C++ \
+             -I _GLIBCXX_NOEXCEPT \
+             -L -
+    done < ./tags-paths
+    mv ./tags ~/.vim/tags
+    echo 'tags+=~/.vim/tags' > ~/.vimrc
+
+fi
+
+cat vimrc_base >> ~/.vimrc
+
