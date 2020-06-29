779f4b6c4e4ed3b81518e8f69f5936771d511728
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2020-06-29 09:32:46 -0700

    updating emacs config

--- shell/init.el
+++ shell/init.el
@@ -13,14 +13,36 @@
 (require 'use-package)
 
 ;; (use-package helm-mode :bind ("C-x C-f" . 'helm-find-files))
-(use-package neotree :bind ("C-x T" . 'neotree))
+(use-package indent-guide
+  :init (indent-guide-global-mode)
+  :ensure t)
+
+(use-package highlight-parentheses
+  :init
+  (highlight-parentheses-mode 1))
+
+(use-package spartparens
+  :ensure t)
+
+(use-package smart-tab
+  :ensure t
+  :init
+  (smart-tab-mode 1))
+
 (use-package multiple-cursors
   :bind ("C-c n" . mc/mark-next-like-this)
         ("C-c p" . mc/mark-previous-like-this)
-        ("C-c a" . mc/mark-all-like-this))
+        ("C-c a" . mc/mark-all-like-this)
+  :init
+  (multiple-cursors-mode 1)
+  :ensure t)
   
 (electric-pair-mode 1)
-(rainbow-delimiters-mode 1)
+
+(use-package rainbow-delimiters
+  :ensure t
+  :init
+  (rainbow-delimiters-mode 1))
 
 ;; Hooks
 (with-eval-after-load 'company
@@ -32,3 +54,17 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
 (setq indent-line-function 'insert-tab)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+    (quote
+     (ivy-posframe highlight-parentheses smart-tab smartparens indent-guide use-package rainbow-delimiters multiple-cursors helm-mode-manager))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
