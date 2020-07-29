9b7a2ef0afe13e839207f67fbc27daa350b7459c
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2020-07-29 13:44:08 -0700

    more aliases

--- templates/aliases.hpp
+++ templates/aliases.hpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
-using ll = long long;
 using l = long;
+using ll = long long;
 using lu = long unsigned;
 using llu = long long unsigned;
 using vi = std::vector<int>;
+using vl = std::vector<int>;
+using vll = std::vector<int>;
 using vd = std::vector<double>;
