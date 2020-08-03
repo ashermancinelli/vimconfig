25c29ebd80a0d9e80ccf74ce6383da170f4d1772
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2020-08-03 10:16:21 -0700

    Added programming competition template

---
+++ templates/compete.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using l = long;
+using ll = long long;
+using lu = long unsigned;
+using llu = long long unsigned;
+using namespace std;
+
+ll xxxxxxxx=0;
+template<class T>
+inline void dbg(T v) std::cout << xxxxxxxx++ << " " << v << '\n';
+
+template<typename T>
+using v = vector<T>;
+template<typename T, int N>
+using ar = array<T, N>;
+
+#define REP(i, n) for(int i=0; i<n; i++)
+
+#if 0
+
+#endif
+
+void solve()
+{
+}
+
+int main()
+{
+  int n; scanf("%d", &n);
+  for(int i=0; i<n; i++)
+  {
+  }
+}
