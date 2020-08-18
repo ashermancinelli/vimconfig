9efffd44d22cb1041d636826e338031ae734af40
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2020-08-18 15:49:50 -0700

    adding dlang for local osx development

---
+++ modules/luamod/WE40281/dlang/2.093.1.lua
@@ -0,0 +1,4 @@
+family("dlang_compiler")
+local home =  "/Users/manc568/workspace/installs/dmd2-2.093.1/"
+prepend_path("PATH", pathJoin(home, "/osx/bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(home, "/osx/lib"))
---
+++ modules/luamod/WE40281/dlang/nightly-08-2020.lua
@@ -0,0 +1,4 @@
+family("dlang_compiler")
+local home =  "/Users/manc568/workspace/installs/dmd2-nightly-8-2020/"
+prepend_path("PATH", pathJoin(home, "/osx/bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(home, "/osx/lib"))
